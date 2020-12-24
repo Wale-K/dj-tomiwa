79d5a745ebee8f50e08c634cd7be817313a1e6aa
Wale-K <olawale.k17@gmail.com>
2020-12-24 16:13:02 +0000

    Added pictures of the people providing testimonies. When you click on them, their testimonial is rendered to the page.

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,44 +1,29 @@
 import React from "react";
 import styled from "styled-components";
-import { icons, colorPalette } from "../utilities";
+import { colorPalette } from "../utilities";
+import hinata from "../images/hinata.jpeg";
+import simon from "../images/simon.jpeg";
+import endeavour from "../images/endeavour.png";
 
 const testimonies = [
   {
-    testimony: (
-      <p>
-        DJ Tomiwa…we want to say THANK YOU SO MUCH for being our DJ for both our
-        traditional and white wedding. Both days were awesome not only because
-        of the songs you played but because of your attitude before and during
-        the big days. It was great to have you on our team and we will
-        definitely be recommending you to everyone!!! Thank you so much!!
-      </p>
-    ),
+    testimony:
+      "DJ Tomiwa…we want to say THANK YOU SO MUCH for being our DJ for both our traditional and white wedding. Both days were awesome not only because of the songs you played but because of your attitude before and during the big days. It was great to have you on our team and we will definitely be recommending you to everyone!!! Thank you so much!!",
     person: <p>KEMI & SENGOVA</p>,
+    picture: hinata,
   },
 
   {
-    testimony: (
-      <p>
-        Hi Tomiwa, I can’t even begin to tell you how much we appreciate you and
-        what you brought to our day. All our guests where so impressed and happy
-        with the songs. You literally catered for every single person that was
-        in the room. I think a few of the aunties said they took your number
-        lool see new business in Bristol. But honestly thank you so much we are
-        so grateful you brought life to our wedding. Much love
-      </p>
-    ),
+    testimony:
+      "Hi Tomiwa, I can’t even begin to tell you how much we appreciate you and what you brought to our day. All our guests where so impressed and happy with the songs. You literally catered for every single person that was in the room. I think a few of the aunties said they took your number lool see new business in Bristol. But honestly thank you so much we are so grateful you brought life to our wedding. Much love.",
     person: <p>JACOB & ALTAR</p>,
+    picture: simon,
   },
   {
-    testimony: (
-      <p>
-        Hey, I should have said thank you by now already but sorry forgot.
-        Massive thanks again. You were so on point!! Lots of people kept banging
-        on about how good the dj was. You adapted really well and got both
-        crowds on their feet. Thanks so much”
-      </p>
-    ),
+    testimony:
+      "Hey, I should have said thank you by now already but sorry forgot. Massive thanks again. You were so on point!! Lots of people kept banging on about how good the dj was. You adapted really well and got both crowds on their feet. Thanks so much.",
     person: <p>KAREN & ADRIAN</p>,
+    picture: endeavour,
   },
 ];
 
@@ -56,21 +41,24 @@ const TestimonialsContainer = styled.div`
   }
 `;
 
-const Test = styled.div`
-  width: 90vw;
-  background-color: blue;
-`;
-
-const TestimoniesCollection = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: 80vw;
-
-  p {
-    margin: 0 10vw;
+const TestimonialsPictures = styled.div`
+  align-self: center;
+  img {
+    height: 5rem;
+    width: 5rem;
+    margin: 2rem 2rem 0 2rem;
   }
 `;
+
+const TestimonyArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 80%;
+  min-height: 40%;
+  background-color: pink;
+`;
+
 class Testimonials extends React.Component {
   state = { testimonyIndex: 0 };
 
@@ -94,14 +82,37 @@ class Testimonials extends React.Component {
     });
   };
 
+  handleToggleTestimonial = (selectedTestimony) => {
+    this.setState({
+      testimonyIndex: selectedTestimony,
+    });
+  };
+
   render() {
     const currentTestimony = testimonies[this.state.testimonyIndex];
     return (
       <TestimonialsContainer>
         <h1>TESTIMONIALS</h1>
+
         <button onClick={this.decrementTesimonyIndex}>PREV</button>
-        {currentTestimony.testimony}
-        {currentTestimony.person}
+        <TestimonyArea>
+          <p>{currentTestimony.testimony}</p>
+          {currentTestimony.person}
+          <TestimonialsPictures>
+            {testimonies.map((testimony, index) => {
+              return (
+                <img
+                  alt=""
+                  key={testimony.person}
+                  src={testimony.picture}
+                  onClick={() => {
+                    this.handleToggleTestimonial(index);
+                  }}
+                />
+              );
+            })}
+          </TestimonialsPictures>
+        </TestimonyArea>
         <button onClick={this.incrementTestimonialsIndex}>NEXT</button>
       </TestimonialsContainer>
     );
